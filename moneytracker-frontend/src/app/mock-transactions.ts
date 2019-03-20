ac4842a28e1a82210e38a5ce048b1f8ecb5b3cf0
Boris Tolstukha <boris.tolstukha@gmail.com>
2019-03-20 23:55:13 +0600

    frontend transactions controller

---
+++ moneytracker-frontend/src/app/mock-transactions.ts
@@ -0,0 +1,21 @@
+import {Transaction} from "./models/Transaction";
+
+export const TRANSACTIONS: Transaction[] = [
+  {
+    id: 1,
+    amount: 200,
+    date: "01.01.2019",
+  },
+  {
+    id: 2,
+    amount: 100,
+    description: "Food",
+    date: "02.01.2019",
+  },
+  {
+    id: 3,
+    amount: 300,
+    description: "Drink",
+    date: "03.01.2019",
+  }
+];
---
+++ moneytracker-frontend/src/app/transactions/transactions.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {TRANSACTIONS} from "../mock-transactions";
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.scss']
+})
+export class TransactionsComponent implements OnInit {
+
+  transactions = TRANSACTIONS;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
